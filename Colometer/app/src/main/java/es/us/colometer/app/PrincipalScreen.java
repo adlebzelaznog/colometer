b8721a49ae4d94948f704653755e5513e39f4f57
David Glez <adelbzelaznog@gmail.com>
2014-05-04 20:15:17 +0200

    Camera preview example implemented. https://github.com/adlebzelaznog/colometer/issues/1 finished

--- Colometer/app/src/main/java/es/us/colometer/app/PrincipalScreen.java
+++ Colometer/app/src/main/java/es/us/colometer/app/PrincipalScreen.java
@@ -1,13 +1,30 @@
 package es.us.colometer.app;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import es.us.colometer.app.camera.CameraManager;
+import es.us.colometer.app.camera.CameraPreview;
 
 
 public class PrincipalScreen extends Activity{
 
+    // Attributes ----------------------------------------------------------------------------------
+    Camera camera;
+    CameraPreview camPreview;
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_welcome);
+
+        // Get a camera instance
+        camera = CameraManager.getCameraInstance();
+
+        // Create a CameraPreview and include it on our activity
+        camPreview = new CameraPreview(this, camera);
+        FrameLayout previewLayout = (FrameLayout) findViewById(R.id.camera_preview);
+        previewLayout.addView(camPreview);
     }
 }
