049279e7f74c359b958dd1ad4086908a0a123b85
David Glez <adelbzelaznog@gmail.com>
2014-05-12 04:19:27 +0200

    Added frame content processing. Apparently not functioning.

--- Colometer/app/src/main/java/es/us/colometer/app/camera/PreviewSurfaceHolder.java
+++ Colometer/app/src/main/java/es/us/colometer/app/camera/PreviewSurfaceHolder.java
@@ -1,5 +1,7 @@
 package es.us.colometer.app.camera;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -8,40 +10,70 @@ import es.us.colometer.app.exceptions.CameraNotAvailableException;
 
 /* Holder for the camera preview surface.
 * This holder allow us to control the images drawn into the surface.*/
-public class PreviewSurfaceHolder  implements SurfaceHolder.Callback {
+public class PreviewSurfaceHolder  implements SurfaceHolder.Callback, Camera.PreviewCallback {
     // Attributes ----------------------------------------------------------------------------------
     private SurfaceHolder holder;
     private Camera camera;
+    private Bitmap image; // image drawn on the surface in each moment
 
+    // Getters and setters -------------------------------------------------------------------------
+    public Bitmap getImage(){
+        return image;
+    }
 
     // Constructor ---------------------------------------------------------------------------------
     public PreviewSurfaceHolder(SurfaceView previewSurface, Camera camera){
         this.camera = camera;
 
+        // Surface holder callback -----------------------------------------
         holder = previewSurface.getHolder();
         holder.addCallback(this);
 
         // deprecated setting, but required on Android versions prior to 3.0
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        // Preview callback ------------------------------------------------
+        //camera.setPreviewCallback(this);
     }
 
 
-    // Callback functionality methods --------------------------------------------------------------
+    // Camera preview callback functionality methods -----------------------------------------------
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        int size = previewSize.height * previewSize.width;
+
+        image = BitmapFactory.decodeByteArray(bytes, 0, size);
+
+        System.out.println("onPreviewFrame called ---------------------------------------");
+    }
+
+
+    // Surface holder callback functionality methods -----------------------------------------------
+    @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
+            camera.setPreviewCallback(this);
+
             camera.setPreviewDisplay(holder);
             camera.getParameters().setPreviewSize(480,640);
             camera.startPreview();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
+            camera.release();
+            camera = null;
+
             throw new CameraNotAvailableException(e.getMessage());
         }
     }
 
+    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
+        // NOTE: Camera is released on the activity
     }
 
+    @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
